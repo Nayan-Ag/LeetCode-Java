3c93f5e40af2b7da7c42827c0a964a5fa8e79033
Nayan Agarwal <108821316+Nayan-Ag@users.noreply.github.com>
2023-06-22 23:23:15 +0530

    Update and rename 199. Binary Tree Right Side View to 199-Binary-Tree-Right-Side-View.java

--- BinaryTree/199-Binary-Tree-Right-Side-View.java
+++ BinaryTree/199-Binary-Tree-Right-Side-View.java
@@ -1,9 +1,9 @@
-Ques: Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+// Ques: Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
 
-Input: root = [1,2,3,null,5,null,4]
-Output: [1,3,4]
+// Input: root = [1,2,3,null,5,null,4]
+// Output: [1,3,4]
 
-Code:
+// Code:
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
 
