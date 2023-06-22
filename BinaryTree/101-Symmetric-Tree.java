d329d8b8d25ffdaa50aa19ea45866bf36cd2a913
Nayan Agarwal <108821316+Nayan-Ag@users.noreply.github.com>
2023-06-22 23:17:59 +0530

    Update and rename 101. Symmetric Tree to 101-Symmetric-Tree.java

--- BinaryTree/101-Symmetric-Tree.java
+++ BinaryTree/101-Symmetric-Tree.java
@@ -1,10 +1,11 @@
-Ques: Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
-Input: root = [1,2,2,3,4,4,3]
-Output: true]
-Input: root = [1,2,2,null,3,null,3]
-Output: false
+// Ques: Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
 
-Code:
+// Input: root = [1,2,2,3,4,4,3]
+// Output: true]
+// Input: root = [1,2,2,null,3,null,3]
+// Output: false
+
+// Code:
 class Solution {
     public boolean isSymmetric(TreeNode root) {
     if (root == null) {
