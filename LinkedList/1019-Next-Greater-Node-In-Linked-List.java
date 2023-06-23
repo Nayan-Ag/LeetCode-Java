ed3f0c26405d4b06120e80c60fb8a00cc3e339e5
Nayan Agarwal <108821316+Nayan-Ag@users.noreply.github.com>
2023-06-23 09:27:37 +0530

    Update and rename 1019. Next Greater Node In Linked List to 1019-Next-Greater-Node-In-Linked-List.java

--- LinkedList/1019-Next-Greater-Node-In-Linked-List.java
+++ LinkedList/1019-Next-Greater-Node-In-Linked-List.java
@@ -1,20 +1,11 @@
-Ques: You are given the head of a linked list with n nodes.
+// Ques: You are given the head of a linked list with n nodes.
 
-For each node in the list, find the value of the next greater node. That is, for each node, find the value of the first node that is next to it and has a strictly larger value than it.
+// For each node in the list, find the value of the next greater node. That is, for each node, find the value of the first node that is next to it and has a strictly larger value than it.
 
-Return an integer array answer where answer[i] is the value of the next greater node of the ith node (1-indexed). If the ith node does not have a next greater node, set answer[i] = 0.
+// Return an integer array answer where answer[i] is the value of the next greater node of the ith node (1-indexed). If the ith node does not have a next greater node, set answer[i] = 0.
 
-Code:
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+// Code:
+
 class Solution {
     public int[] nextLargerNodes(ListNode head) {
         int size = size(head);
