8bf68cbeaadf5dc4c2e9df24b4b0713bf9bd1fac
Nayan Agarwal <108821316+Nayan-Ag@users.noreply.github.com>
2023-06-24 22:11:13 +0530

    Update and rename Kth Largest Element in an Array to Kth-Largest-Element-in-an-Array.java

--- Priority Queue/Kth-Largest-Element-in-an-Array.java
+++ Priority Queue/Kth-Largest-Element-in-an-Array.java
@@ -1,7 +1,7 @@
-Ques: Given an integer array nums and an integer k, return the kth largest element in the array.
-Note that it is the kth largest element in the sorted order, not the kth distinct element.
+// Ques: Given an integer array nums and an integer k, return the kth largest element in the array.
+// Note that it is the kth largest element in the sorted order, not the kth distinct element.
 
-Code:
+// Code:
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         PriorityQueue<Integer> minheap = new PriorityQueue<>();
