a74d7012425e6fdc3f89ca13ec56e86d2f084223
Nayan Agarwal <108821316+Nayan-Ag@users.noreply.github.com>
2023-06-25 10:31:41 +0530

    Update and rename 1572. Matrix Diagonal Sum to 1572. Matrix Diagonal Sum.java

--- Matrix/1572. Matrix Diagonal Sum.java
+++ Matrix/1572. Matrix Diagonal Sum.java
@@ -1,8 +1,8 @@
-Ques: Given a square matrix mat, return the sum of the matrix diagonals.
+// Ques: Given a square matrix mat, return the sum of the matrix diagonals.
 
-Only include the sum of all the elements on the primary diagonal and all the elements on the secondary diagonal that are not part of the primary diagonal.
+// Only include the sum of all the elements on the primary diagonal and all the elements on the secondary diagonal that are not part of the primary diagonal.
 
-Code: 
+// Code: 
 class Solution {
     public int diagonalSum(int[][] mat) {
         int sum = 0;
