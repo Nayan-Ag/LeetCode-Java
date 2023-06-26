2bc1990ba1c029ba138c3e105d6245f0da04419d
Nayan Agarwal <108821316+Nayan-Ag@users.noreply.github.com>
2023-06-26 17:04:14 +0530

    Update and rename Cells with Odd Values in a Matrix to Cells with Odd Values in a Matrix.java

--- Matrix/Cells with Odd Values in a Matrix.java
+++ Matrix/Cells with Odd Values in a Matrix.java
@@ -1,12 +1,13 @@
-Ques: There is an m x n matrix that is initialized to all 0's. There is also a 2D array indices where each indices[i] = [ri, ci] represents a 0-indexed location to perform some increment operations on the matrix.
+// Ques: There is an m x n matrix that is initialized to all 0's. There is also a 2D array indices where each indices[i] = [ri, ci] represents a 0-indexed location to perform some increment operations on the matrix.
 
-For each location indices[i], do both of the following:
+// For each location indices[i], do both of the following:
 
-Increment all the cells on row ri.
-Increment all the cells on column ci.
-Given m, n, and indices, return the number of odd-valued cells in the matrix after applying the increment to all locations in indices.
+// Increment all the cells on row ri.
+// Increment all the cells on column ci.
+// Given m, n, and indices, return the number of odd-valued cells in the matrix after applying the increment to all locations in indices.
 
-Code:
+// Code:
+
 class Solution {
     public int oddCells(int m, int n, int[][] indices) {
         int[][] arr = new int[m][n];
